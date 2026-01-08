ec1f41c814708b6f0b56715c94dfa1d330de4cc5
ezy4me <82320292+ezy4me@users.noreply.github.com>
2026-01-08 15:56:58 +0300

    init

---
+++ Group4337/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Group4337
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
